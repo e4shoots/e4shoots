c7cef54f42d39b09b2fc3c856226ed0f487b44d7
e4shoots <enuiaola@gmail.com>
2026-03-24 22:49:50 -1000

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -279,3 +279,23 @@
 				});
 
 })(jQuery);
+
+<script>
+const items = document.querySelectorAll(".faq-item");
+
+items.forEach(item => {
+  const button = item.querySelector(".faq-question");
+
+  button.addEventListener("click", () => {
+    item.classList.toggle("active");
+
+    const answer = item.querySelector(".faq-answer");
+
+    if (item.classList.contains("active")) {
+      answer.style.maxHeight = answer.scrollHeight + "px";
+    } else {
+      answer.style.maxHeight = null;
+    }
+  });
+});
+</script>
